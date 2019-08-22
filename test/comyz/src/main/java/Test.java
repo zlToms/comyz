27097e949bda94e675806e498217d6197f8fba02
zlToms <52475751+zlToms@users.noreply.github.com>
2019-08-22 08:40:40 +0800

    Update Test.java
    学习1

--- test/comyz/src/main/java/Test.java
+++ test/comyz/src/main/java/Test.java
@@ -1,5 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("你好! version merge  V.07   合并分支处理  +github 编辑");
+        System.out.println("你好! version merge  V.07   合并分支处理  +github 编辑 +jdshfhds");
     }
 }
