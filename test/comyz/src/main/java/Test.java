942a3b8b081d1fd3bdc5704f56a67a070e459885
zlToms <253628393@qq.com>
2019-08-22 08:54:11 +0800

    v.12

--- test/comyz/src/main/java/Test.java
+++ test/comyz/src/main/java/Test.java
@@ -2,6 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("你好! version merge  V.07   合并分支处理  +github 编辑 =flkdgnflkd");
         System.out.println("你好! version merge  V.07   合并分支处理  +github 编辑 +jdshfhds");
-        System.out.println("learning version 1");
+        System.out.println("learning version 1 +sfjs0.2");
     }
 }
